04ae1cc36de0f645f748c5b80d5d306cb998fc0b
Nyrek2001 <Nyrekwheeler@gmail.com>
2025-04-15 18:59:16 -0400

    first commit

---
+++ target/debug/deps/supplement_p2_10-a2b972eb01d3f76c.d
@@ -0,0 +1,5 @@
+C:\Users\mcelr\supplement_p2_10\target\debug\deps\supplement_p2_10-a2b972eb01d3f76c.exe: src\lib.rs
+
+C:\Users\mcelr\supplement_p2_10\target\debug\deps\supplement_p2_10-a2b972eb01d3f76c.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/supplement_p2_10-aac286001f24e97a.d
@@ -0,0 +1,7 @@
+C:\Users\mcelr\supplement_p2_10\target\debug\deps\libsupplement_p2_10-aac286001f24e97a.rmeta: src\lib.rs
+
+C:\Users\mcelr\supplement_p2_10\target\debug\deps\libsupplement_p2_10-aac286001f24e97a.rlib: src\lib.rs
+
+C:\Users\mcelr\supplement_p2_10\target\debug\deps\supplement_p2_10-aac286001f24e97a.d: src\lib.rs
+
+src\lib.rs:
